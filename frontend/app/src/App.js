6ff67de17e6983dd110376fd518f042bae1a36d7
Prosper Ndebele <pndebele@wesleyan.edu>
2025-04-21 11:28:01 -0400

    used HashRouter

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoutes";
 import Login from "./pages/Login";
 import LandingPage from "./pages/LandingPage";
